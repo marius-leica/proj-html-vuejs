54c8b794576d9aa609548d1d99845e62b26ac724
Marius L <mariusleica@gmail.com>
2022-07-04 20:46:56 +0200

    finish another section

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -14,9 +14,18 @@
                             There live the blind Texts. Separated they live in Bookmarksgrove right at the coast of the
                             Semantics, a large language ocean. Separated they live in Bookmarksgrove.</p>
                         <div class="btn my-btn btn-black-gradient my-5">READ MORE</div>
-                        <div class="d-flex justify-content-between">
-                            <h5 class="pt-5 pb-4">FACEBOOK - INSTAGRAM - YOUTUBE - TWITTER</h5>
-                            <div class="btn btn-black-gradient d-flex h-100 mt-5 rounded-pill p-0">
+                        <div class="d-flex justify-content-between align-items-baseline">
+                            <h5 class="pt-5 pb-4 social-links">
+                                <a target="_blank" class="text-muted text-decoration-none green-link"
+                                    href="https://it-it.facebook.com/">FACEBOOK</a> -
+                                <a target="_blank" class="text-muted text-decoration-none"
+                                    href="https://www.instagram.com/">INSTAGRAM</a> -
+                                <a target="_blank" class="text-muted text-decoration-none"
+                                    href="https://www.youtube.com/">YOUTUBE</a> -
+                                <a target="_blank" class="text-muted text-decoration-none"
+                                    href="https://twitter.com/">TWITTER</a>
+                            </h5>
+                            <div class="btn btn-black-gradient d-flex h-100 rounded-pill p-0">
                                 <div class="text-center py-2 px-3 color-gray">01</div>
                                 <div class="text-center py-2 px-3 rounded-pill btn-green-gradient color-gray">
                                     02</div>
@@ -25,26 +34,120 @@
                         </div>
                     </div>
                     <div class="col">
-                        <img class="position-absolute hero-img" src="/img/Group-35-2x.png" alt="">
+                        <img class="position-absolute top-50 translate-middle-y hero-img img-fluid"
+                            src="/img/Group-35-2x.png" alt="">
                     </div>
                 </div>
             </div>
         </section>
-        <section class="bg-light-gray mt-5 pt-5 pb-3 mb-3">
-            <div class="container">
-                <h2>We Are Here To Make Your <span class="color-green-fluo">Website</span> Look More
-                    <span class="color-green-fluo">Elegant</span> And Stylish!
-                </h2>
-                <div class="underline-container">
-                    <div class="underline-small"></div>
-                    <div class="underline-big"></div>
+        <section class="mt-5 pt-5 pb-3 mb-3">
+            <div class="container position-relative">
+                <div class="row">
+                    <div class="col col-4">
+                        <div class="ot-5">
+                            <h2>We Are Here To<br> Make Your <span class="color-green-fluo">Website</span><br> Look More
+                                <span class="color-green-fluo">Elegant</span><br> And Stylish!
+                            </h2>
+                            <div class="underline-container">
+                                <div class="underline-small"></div>
+                                <div class="underline-big"></div>
+                            </div>
+                            <div class="btn my-btn btn-green-gradient">VIEW ALL</div>
+                        </div>
+                    </div>
+                    <div class="col col-8">
+                        <div class="row row-cols-2 mb-5">
+                            <div class="col">
+                                <div class="card px-3 py-4 shadow my-green-card">
+                                    <div class=" card-body">
+                                        <i class="fa-solid fa-gauge-high color-green-fluo mb-4"></i>
+                                        <h5 class="card-title">Speed Optimisation</h5>
+                                        <p class="card-text">Far far away, behind the word mountains, far from the
+                                            countries
+                                            Vokalia Separated...</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card px-3 py-4 shadow my-green-card">
+                                    <div class="card-body">
+                                        <i class="fa-solid fa-cloud color-green-fluo mb-4"></i>
+                                        <h5 class="card-title">Cloud Solutins</h5>
+                                        <p class="card-text">Far far away, behind the word mountains, far from the
+                                            countries
+                                            Vokalia Separated...</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-cols-2">
+                            <div class="col">
+                                <div class="card px-3 py-4 shadow my-green-card">
+                                    <div class="card-body">
+                                        <i class="fa-solid fa-tablet-screen-button color-green-fluo mb-4"></i>
+                                        <h5 class="card-title">Website Design</h5>
+                                        <p class="card-text">Far far away, behind the word mountains, far from the
+                                            countries
+                                            Vokalia Separated...</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card px-3 py-4 shadow my-green-card">
+                                    <div class="card-body">
+                                        <i class="fa-solid fa-stopwatch color-green-fluo mb-4"></i>
+                                        <h5 class="card-title">Online Marketing</h5>
+                                        <p class="card-text">Far far away, behind the word mountains, far from the
+                                            countries
+                                            Vokalia Separated...</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="vertical-p">17 yeasr of experience helping people
+                            for best solutions</div>
+                    </div>
                 </div>
-                <div class="btn my-btn btn-green-gradient d-none d-lg-block ">VIEW ALL</div>
             </div>
 
+
+
         </section>
-        <!-- sezzione  -->
-        <!-- sezzione learn more about our missions -->
+        <section class="bg-light-gray mt-5 pt-5 pb-8 mb-3 overflow-hidden position-relative">
+            <div class="container">
+                <div class="row">
+                    <div class="col col-8">
+                        <div>
+                            <!-- <img src="/img/businesswoman-analysing-document-P8WSNMC-1024x820.jpg" alt=""> -->
+                            <div class="green-filter-img " :style="{ 'background-image': 'url(' + greenImg + ')' }">
+                                <div class="overlay-green-filter"></div>
+                            </div>
+                            <div>
+                                <img class="img-woman" src="/img/businesswoman-analysing-document-P8WSNMC-1024x820.jpg"
+                                    alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col col-4 pt-5 pb-5 mb-5">
+                        <div class="pt-5 pb-5 mb-5">
+                            <h2>learn more<br> about our <span class="color-green-fluo">missions</span>
+                            </h2>
+                            <div class="underline-container">
+                                <div class="underline-small"></div>
+                                <div class="underline-big"></div>
+                            </div>
+                            <p> Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                                There live the blind Texts. Separated they live in Bookmarksgrove right at the coast of
+                                the
+                                Semantics, a large language ocean. Separated they live in Bookmarksgrove.</p>
+                            <div class="btn my-btn btn-green-gradient">READ MORE</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
         <!-- sezzione Our recenent web designs & some examples of past projects -->
         <!-- sezzione We Love What We Do -->
         <!-- sezzione With Live Chat 24/7 Support -->
@@ -60,6 +163,7 @@
 export default {
     data() {
         return {
+            greenImg: '/img/busy-architect-PYVKWM4-1024x872.jpg',
 
         }
     },
@@ -68,16 +172,72 @@ export default {
 
 </script>
 
-<style lang=" scss" scoped>
-
+<style lang="scss" scoped>
+@import "../assets/scss/variables";
 @import "../assets/scss/btn";
 @import "../assets/scss/typography";
-@import "../assets/scss/variables";
 @import "../assets/scss/utility";
 
 .hero-img {
-width: 60%;
-top: -35%;
-left: 49%;
+    width: 60vw;
+    right: -15% !important;
+}
+
+h5 .green-link:hover {
+    color: $green-fluo;
+}
+
+.my-green-card .card-body .fa-solid {
+    font-size: 3.5rem;
+}
+
+.my-green-card:hover {
+    background: linear-gradient(90deg, $green-lemon 0%, $green-fluo 45%);
+    box-shadow: 5px 5px 30px $green-fluo;
+    color: white;
+    cursor: pointer;
+
+
+
+    .card-body .fa-solid {
+        color: white;
+    }
+}
+
+.vertical-p {
+    position: absolute;
+    top: -40%;
+    left: 50%;
+    width: 100%;
+    transform: rotate(270deg);
+}
+
+.overlay-green-filter {
+    width: 100%;
+    height: 100%;
+    border-radius: 0 5% 5% 0;
+    background-color: rgba($green-fluo, 0.8);
+}
+
+.green-filter-img {
+    width: 40vw;
+    aspect-ratio: 2/1.5;
+    position: absolute;
+    left: 0;
+    top: 15%;
+    border-radius: 0 5% 5% 0;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+
+.img-woman {
+    position: absolute;
+    object-fit: cover;
+    width: 40%;
+    aspect-ratio: 2/1.5;
+    top: 25%;
+    left: 11%;
+    border-radius: 5%;
 }
 </style>
